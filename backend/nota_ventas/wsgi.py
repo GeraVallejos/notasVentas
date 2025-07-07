8536c575ca87e2f5dc242ebe9b0842a437d686e0
GeraVallejos <ger.vallejos@hotmail.com>
2025-07-07 11:29:24 -0400

    Arreglo en wsgi

--- backend/nota_ventas/wsgi.py
+++ backend/nota_ventas/wsgi.py
@@ -10,10 +10,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nota_ventas.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'backend', 'staticfiles'))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 
 
